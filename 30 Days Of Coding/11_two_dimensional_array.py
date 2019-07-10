7e04eaa5ebde109b53416c4e17acfb75ce03cf99
adnanobot <33164284+adnanobot@users.noreply.github.com>
2019-07-10 17:03:42 +0200

    Add files via upload

---
+++ 30 Days Of Coding/11_two_dimensional_array.py
@@ -0,0 +1,32 @@
+"""
+    Day 7 of 30 days of coding from hackerrank.com : Let's review
+
+    author : adnanobot
+
+    Arrays
+    conditions : size of array : 1 <= N <= 1000
+                 value of the element of the array : 1 <= N <= 10000
+"""
+
+arr =[[-1, -1, 0, -9, -2, -2],
+     [-2, -1, -6, -8, -2, -5],
+     [-1, -1, -1, -2, -3, -4],
+     [-1, -9, -2, -4, -4, -5],
+     [-7, -3, -3, -2, -9, -9],
+     [-1, -3, -1, -2, -4, -5]]
+
+sum = 0
+sums = []
+for i in range(0, 4):
+  print("new pyramid")
+
+  for j in range(0, 4):
+    for k in range(j, j+3):
+      sum += arr[i][k] + arr[i+2][k]
+
+    sum += arr[i+1][j+1]
+    print("sum: ", sum)
+    sums.append(sum)
+    sum = 0
+
+print("maximum sum is: ", max(sums))
